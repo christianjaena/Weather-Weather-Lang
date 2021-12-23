07d3b1c26b5b349fc3b424af4037b2992cc82aa7
christianjaena <christian.jaena@msugensan.edu.ph>
2021-12-23 15:04:31 +0800

    integrated scripts to template; added favicon

--- client/auth.js
+++ client/auth.js
@@ -1,7 +1,7 @@
-window.onload = async () => {
+(async () => {
   let getRequest = await fetch('http://localhost:3000/auth');
   let response = await getRequest;
   if (response.redirected) {
     window.location.href = response.url;
   }
-};
+})();
