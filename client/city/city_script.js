53c2086ee280a5a88aaa159ab0579927dda3673c
christianjaena <christian.jaena@msugensan.edu.ph>
2022-01-15 00:55:55 +0800

    fixed responsive issues on city weather details page

--- client/city/city_script.js
+++ client/city/city_script.js
@@ -1,6 +1,7 @@
 import { postHTTPRequest } from '../libraries/postHTTPRequest.js';
 let backButton = document.getElementById('back');
 let favoritesButton = document.getElementById('favorite');
+let weatherOverview = document.getElementById('weatherOverview');
 let userID = localStorage.getItem('userID');
 let woeid = localStorage.getItem('woeid');
 let location = '';
@@ -26,7 +27,7 @@ backButton.addEventListener('click', () => {
 });
 
 (async () => {
-  results.innerHTML = '<h1>Loading Data...</h1>';
+  weatherOverview.innerHTML = '<h1>Loading Data...</h1>';
   let res = await postHTTPRequest(
     'http://localhost:3000/weather/info/location',
     {
@@ -36,11 +37,13 @@ backButton.addEventListener('click', () => {
   // LINQ - arr.first()
   let data = res.first();
   location = `${data.title} ${data.location_type}`;
-  results.innerHTML = `
+  weatherOverview.innerHTML = `
+    <div class="weatherInfoContainer">
       <h2 class="location">${data.parent.title}</h2>
       <h4>${location}</h4>
       <p>Timezone: ${data.timezone}</p>
       <p>Coordinates: ${data.latt_long}</p>
+    </div>
       `;
   // LINQ - arr.take(), arr.select(), arr.first();
   let weatherInfo = res
@@ -49,33 +52,24 @@ backButton.addEventListener('click', () => {
     .first();
   weatherInfo.forEach((day) => {
     results.innerHTML += `
- 
-    <div class="main">
-      <div class="container3"> 
-          <h1 > ${day.applicable_date}</h1><br />
-          
-          Weather Condition: ${day.weather_state_name}
-
+      <div class="dayWeather">
+          <h1> ${day.applicable_date}</h1>
+          <p>Weather Condition: ${day.weather_state_name}</p>
           <img src="https://www.metaweather.com/static/img/weather/${
-              day.weather_state_abbr
-            }.svg" height="100" width="100"/> <br /> <br />
-            
-            Predictability:${day.predictability}<br />
-            Humidity:${day.humidity}<br />
-            Avg Temperature: ${day.the_temp.toFixed(2)}<br />
-            Max Temperature: ${day.max_temp.toFixed(2)}<br />
-            MinTemperature: ${day.min_temp.toFixed(2)}<br />
-            Wind Speed:  ${day.wind_speed.toFixed(2)}<br />
-
-            Wind Direction: ${day.wind_direction.toFixed(2)} ${
-              day.wind_direction_compass
-            }<br />
-            
-            Air Pressure:${day.air_pressure} <br />
-            Visibility: ${day.visibility.toFixed(2)}
-            </div>
-        </div>  
- 
+            day.weather_state_abbr
+          }.svg" height="100" width="100"/>
+          <p>Predictability:${day.predictability}</p>
+          <p>Humidity:${day.humidity}</p>
+          <p>Avg Temperature: ${day.the_temp.toFixed(2)}</p>
+          <p>Max Temperature: ${day.max_temp.toFixed(2)}</p>
+          <p>MinTemperature: ${day.min_temp.toFixed(2)}</p>
+          <p>Wind Speed:  ${day.wind_speed.toFixed(2)}</p>
+          <p>Wind Direction: ${day.wind_direction.toFixed(2)} ${
+      day.wind_direction_compass
+    }</p>
+          <p>Air Pressure:${day.air_pressure}</p>
+          <p>Visibility: ${day.visibility.toFixed(2)}</p>
+      </div>
     `;
   });
 })();
